17eb1dc30a109481566c20a93db5e0d0a8d6abf5
PRODYNA+jlong <jie.long@prodyna.com>
2015-09-30 13:29:28 +0000

    project initialization

---
+++ src/main/java/org/shelferp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.shelferp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProductApplication.class);
+	}
+
+}
